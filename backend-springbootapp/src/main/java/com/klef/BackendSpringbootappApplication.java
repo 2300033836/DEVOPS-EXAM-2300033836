1b95c5dd1cc8bed7881493f7ee6ceaa49f025880
2300033836 <kallepallisushmasri@gmail.com>
2025-10-18 11:19:13 +0530

    project v1.0

--- backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
+++ backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendSpringbootappApplication {
+public class BackendSpringbootappApplication extends SpringBootServletInitializer {
 
+
+     @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BackendSpringbootappApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(BackendSpringbootappApplication.class, args);
 		System.out.println("Spring Boot Project is Running ...");
